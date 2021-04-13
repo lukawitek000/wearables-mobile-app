cf4195bba35450ad818d17e966665cedb078a511
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-13 19:44:10 +0200

    set up bottom navigation and create fragments to the views

--- app/src/main/java/com/erasmus/upv/eps/wearables/MainActivity.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/MainActivity.kt
@@ -3,15 +3,20 @@ package com.erasmus.upv.eps.wearables
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +24,27 @@ class MainActivity : AppCompatActivity() {
         setUpNavController()
         setUpActionBar()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setupWithNavController(navController)
+        controlBottomNavigationVisibility()
     }
 
     private fun setUpActionBar() {
-        val appBarConfiguration = AppBarConfiguration
-            .Builder(R.id.splashScreenFragment, R.id.tutorialFragment, R.id.matchesFragment)
-            .build()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.splashScreenFragment,
+                R.id.tutorialFragment,
+                R.id.matchesFragment,
+                R.id.teamsFragment,
+                R.id.playersFragment,
+                R.id.infoFragment
+            )
+        )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         controlActionBarVisibility()
     }
@@ -38,10 +58,19 @@ class MainActivity : AppCompatActivity() {
     private fun controlActionBarVisibility() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.splashScreenFragment -> {
-                    supportActionBar?.hide()
-                }
+                R.id.splashScreenFragment -> supportActionBar?.hide()
                 else -> supportActionBar?.show()
+            }
+        }
+    }
+
+
+    private fun controlBottomNavigationVisibility(){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashScreenFragment -> bottomNavigation.visibility = View.GONE
+                R.id.tutorialFragment -> bottomNavigation.visibility = View.GONE
+                else -> bottomNavigation.visibility = View.VISIBLE
             }
         }
     }
