b22c4ed72f503f88536781016b804666c55f4f71
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-13 21:59:00 +0200

    add statistics screen and navigation to it

--- app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/PastMatchesFragment.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/PastMatchesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.erasmus.upv.eps.wearables.R
 
 
@@ -18,8 +20,13 @@ class PastMatchesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_past_matches, container, false)
+        val view =  inflater.inflate(R.layout.fragment_past_matches, container, false)
+
+        view.findViewById<Button>(R.id.select_some_match_bt).setOnClickListener {
+            findNavController().navigate(R.id.action_matchesFragment_to_matchStatisticsFragment)
+        }
+
+        return view
     }
 
 
