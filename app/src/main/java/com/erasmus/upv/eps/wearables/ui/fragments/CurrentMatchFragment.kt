31564d711d548e2c051342adff91e3dee5794038
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-14 13:03:22 +0200

    add dialogs for choosing team and players

--- app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/CurrentMatchFragment.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/CurrentMatchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.erasmus.upv.eps.wearables.R
 
 
@@ -13,7 +14,18 @@ class CurrentMatchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_current_match, container, false)
+        val view =  inflater.inflate(R.layout.fragment_current_match, container, false)
+
+        view.findViewById<Button>(R.id.ask_team_bt).setOnClickListener {
+            DialogWhichTeamFragment().show(childFragmentManager, DialogWhichTeamFragment.TAG)
+        }
+
+
+        view.findViewById<Button>(R.id.ask_player_bt).setOnClickListener {
+            DialogWhichPlayerFragment().show(childFragmentManager, DialogWhichPlayerFragment.TAG)
+        }
+
+        return view
     }
 
 
---
+++ app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/DialogWhichTeamFragment.kt
@@ -0,0 +1,29 @@
+package com.erasmus.upv.eps.wearables.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.erasmus.upv.eps.wearables.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class DialogWhichTeamFragment : BottomSheetDialogFragment() {
+
+    companion object{
+        const val TAG = "DialogWhichTeamFragment"
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dialog_which_team, container, false)
+    }
+
+
+}
