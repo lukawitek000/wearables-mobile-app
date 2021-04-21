a50bbc358a8550aae52c9d8958a6294b44733fbd
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-21 20:51:34 +0200

    get data from create player layout and pass it to viewmodel

--- app/src/main/java/com/erasmus/upv/eps/wearables/model/Player.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/model/Player.kt
@@ -1,11 +1,11 @@
 package com.erasmus.upv.eps.wearables.model
 
 data class Player(
-    val id: String,
-    val name: String,
-    val sport: String,
-    val number: Int,
-    val position: String,
-    val otherInfo: String = "", // default value just for now, because of fake data
-    val photo: String = "" // TODO add taking photos or getting photos from device
+    var id: String,
+    var name: String,
+    var sport: String,
+    var number: Int,
+    var position: String,
+    var otherInfo: String = "", // default value just for now, because of fake data
+    var photo: String = "" // TODO add taking photos or getting photos from device
 )
--- app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/CreatePlayerFragment.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/CreatePlayerFragment.kt
@@ -1,22 +1,81 @@
 package com.erasmus.upv.eps.wearables.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.erasmus.upv.eps.wearables.R
+import com.erasmus.upv.eps.wearables.databinding.FragmentCreatePlayerBinding
+import com.erasmus.upv.eps.wearables.viewModels.CreatePlayerViewModel
 
 
 class CreatePlayerFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentCreatePlayerBinding
+
+    private val viewModel: CreatePlayerViewModel by viewModels()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_create_player, container, false)
+    ): View {
+        binding = FragmentCreatePlayerBinding.inflate(
+                inflater, container, false
+        )
+
+        getUserInput()
+
+
+        return binding.root
     }
+
+    private fun getUserInput() {
+        getRadioButtonInput()
+        setSavePlayerListener()
+    }
+
+
+    private fun getRadioButtonInput(){
+        setPlayersSportBaseOnRadioButtonChecked(binding.sportRadioGroup.checkedRadioButtonId)
+        binding.sportRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            setPlayersSportBaseOnRadioButtonChecked(checkedId)
+        }
+    }
+
+    private fun setPlayersSportBaseOnRadioButtonChecked(checkedId: Int) {
+        when (checkedId) {
+            R.id.football_radio_button -> viewModel.setPlayersSport("Football")
+            R.id.handball_radio_button -> viewModel.setPlayersSport("Handball")
+            R.id.basketball_radio_button -> viewModel.setPlayersSport("Basketball")
+        }
+    }
+
+    private fun setSavePlayerListener(){
+        binding.savePlayerBt.setOnClickListener {
+            viewModel.createPlayer(
+                    binding.playerNameEt.text.toString(),
+                    getPlayerNumber(),
+                    binding.playerPositionEt.text.toString(),
+                    binding.playerOtherInfoEt.text.toString()
+            )
+            Log.i("CreatePlayerFragment", "setSavePlayerListener: ${viewModel.player}")
+        }
+    }
+
+    private fun getPlayerNumber(): Int {
+        return try {
+            binding.playerNumberEt.text.toString().toInt()
+        }catch (e: Exception){
+            0
+        }
+    }
+
+
 
 
 }
