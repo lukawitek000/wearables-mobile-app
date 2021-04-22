fcd7458eca561cb82888e927daf68205fd6f0177
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-22 12:06:58 +0200

    fix team dao and db

--- app/src/main/java/com/erasmus/upv/eps/wearables/db/AppDatabase.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/db/AppDatabase.kt
@@ -7,9 +7,10 @@ import androidx.room.RoomDatabase
 import com.erasmus.upv.eps.wearables.db.dao.PlayerDao
 import com.erasmus.upv.eps.wearables.db.dao.TeamDao
 import com.erasmus.upv.eps.wearables.model.Player
+import com.erasmus.upv.eps.wearables.model.Team
 
 
-@Database(entities = [Player::class], version = 2)
+@Database(entities = [Player::class, Team::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun playerDao(): PlayerDao
--- app/src/main/java/com/erasmus/upv/eps/wearables/db/dao/TeamDao.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/db/dao/TeamDao.kt
@@ -1,8 +1,10 @@
 package com.erasmus.upv.eps.wearables.db.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import com.erasmus.upv.eps.wearables.model.Team
 
+@Dao
 interface TeamDao {
 
 
