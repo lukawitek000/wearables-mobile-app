b96fb072369944717e8f44ad11ecc46e83d35d45
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-22 12:36:00 +0200

    implement inserting team to the database

--- app/src/main/java/com/erasmus/upv/eps/wearables/repositories/TeamRepository.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/repositories/TeamRepository.kt
@@ -1,8 +1,18 @@
 package com.erasmus.upv.eps.wearables.repositories
 
 import com.erasmus.upv.eps.wearables.db.dao.TeamDao
+import com.erasmus.upv.eps.wearables.model.Team
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TeamRepository (
     private val teamDao: TeamDao
         ) {
+
+    suspend fun saveTeam(team: Team){
+        withContext(Dispatchers.IO){
+            teamDao.insertTeam(team)
+        }
+    }
+
 }
--- app/src/main/java/com/erasmus/upv/eps/wearables/viewModels/CreateTeamViewModel.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/viewModels/CreateTeamViewModel.kt
@@ -2,9 +2,11 @@ package com.erasmus.upv.eps.wearables.viewModels
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.erasmus.upv.eps.wearables.model.Team
 import com.erasmus.upv.eps.wearables.repositories.TeamRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,8 +14,13 @@ class CreateTeamViewModel
 @Inject constructor(
     private val repository: TeamRepository
 ) : ViewModel(){
+
+
     fun saveTeam(team: Team) {
         Log.i("CreateTeamViewModel", "saveTeam: $team ")
+        viewModelScope.launch {
+            repository.saveTeam(team)
+        }
     }
 
 }
