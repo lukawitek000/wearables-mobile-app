44bd01480bb791b3cd9908ee2b7adcca82bb2914
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-04-22 11:55:42 +0200

    update team properties and fix errors connected with that

--- app/src/main/java/com/erasmus/upv/eps/wearables/model/Team.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/model/Team.kt
@@ -2,15 +2,16 @@ package com.erasmus.upv.eps.wearables.model
 
 import android.graphics.Color
 import android.net.Uri
+import androidx.room.PrimaryKey
 
 data class Team (
-    val id: String,
+    val id: Long,
     val name: String,
-    val players: List<Player>,
+    //val players: List<Player>,
     val sport: String,
     val color: Int,
     val country: String,
     val city: String,
-    val logo: Uri?,
+   // val logo: Uri?,
     val others: String
 )
