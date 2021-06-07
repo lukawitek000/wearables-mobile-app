08ca8149d208570a363fdfed33064810fa505d3b
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-06-07 16:24:13 +0200

    team info into dialog

--- app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/TeamsFragment.kt
+++ app/src/main/java/com/erasmus/upv/eps/wearables/ui/fragments/TeamsFragment.kt
@@ -1,5 +1,6 @@
 package com.erasmus.upv.eps.wearables.ui.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +16,7 @@ import com.erasmus.upv.eps.wearables.R
 import com.erasmus.upv.eps.wearables.databinding.FragmentTeamsBinding
 import com.erasmus.upv.eps.wearables.model.Team
 import com.erasmus.upv.eps.wearables.ui.adapters.TeamsAdapter
+import com.erasmus.upv.eps.wearables.util.DialogBuilder
 import com.erasmus.upv.eps.wearables.util.TeamCreated
 import com.erasmus.upv.eps.wearables.viewModels.CreateRelationsViewModel
 import com.erasmus.upv.eps.wearables.viewModels.TeamsViewModel
@@ -104,8 +106,57 @@ class TeamsFragment : Fragment() {
     }
 
     private fun onClickTeamInfo(team: Team){
-        val direction = TeamsFragmentDirections.actionTeamsFragmentToTeamInfoFragment(team.teamId)
+//        val direction = TeamsFragmentDirections.actionTeamsFragmentToTeamInfoFragment(team.teamId)
+//        findNavController().navigate(direction)
+        DialogBuilder.buildAndShowDialog(
+            requireContext(),
+            requireActivity().layoutInflater,
+            "Team action",
+            "What do you want to do with this Team?",
+            yesButtonText = "Delete",
+            noButtonText = "Update",
+            yesButtonAction = { dialog ->
+                handleDeleteButton(team.teamId)
+                dialog.dismiss()
+            },
+            noButtonAction = { dialog ->
+                navigateToUpdateTheTeam(team.teamId)
+                dialog.dismiss()
+            }
+        )
+
+    }
+
+    private fun navigateToUpdateTheTeam(teamId: Long) {
+        val direction = TeamsFragmentDirections.actionTeamsFragmentToCreateTeamFragment(teamId)
         findNavController().navigate(direction)
+    }
+
+    private fun handleDeleteButton(teamId: Long) {
+        viewModel.isTeamDeleted.observe(viewLifecycleOwner){
+            if(!it){
+                showDialogInformingThatTeamIsPartOfMatch()
+            }
+        }
+        viewModel.deleteTeamById(teamId)
+
+    }
+
+    private fun showDialogInformingThatTeamIsPartOfMatch() {
+        DialogBuilder.buildAndShowDialog(
+            requireContext(),
+            requireActivity().layoutInflater,
+            "Cannot delete the team",
+            "The team is part of a match, so it cannot be deleted",
+            yesButtonText = getString(R.string.ok),
+            yesButtonAction = { dialog -> dialog.dismiss() },
+            isNoButtonVisible = false
+        )
+//        val builder = AlertDialog.Builder(requireContext())
+//        builder.setTitle("Cannot delete the team")
+//        builder.setMessage("The team is part of a match, so it cannot be deleted")
+//        builder.setPositiveButton("Ok") { dialog, _ -> dialog.dismiss() }
+//        builder.create().show()
     }
 
 
